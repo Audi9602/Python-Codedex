53dd4bc1bd617124a9369ce0cd7945a2b090237e
Addie@the19 <95099812+Audi9602@users.noreply.github.com>
2026-04-24 17:35:30 +0530

    Create stonks.py

---
+++ stonks.py
@@ -0,0 +1,20 @@
+stock_prices = [34.68, 36.09, 34.94, 33.97, 34.68, 35.82, 43.41, 44.29, 44.65, 53.56, 49.85, 48.71, 48.71, 49.94, 48.53, 47.03, 46.59, 48.62, 44.21, 47.21]
+
+def price_at(x):
+  return stock_prices[x - 1]
+
+def max_price(x, y):
+  mx = 0
+  for i in range(x, y + 1):
+    mx = max(mx, price_at(i))
+  return mx
+
+def min_price(x, y):
+  mn = price_at(x)
+  for i in range(x, y + 1):
+    mn = min(mn, price_at(i))
+  return mn
+
+print(max_price(1, 15))
+print(min_price(5, 10))
+print(price_at(3))
