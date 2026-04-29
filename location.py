edc572e8ca09dcb2e76166f8c72eba4f6d31d04e
Addie@the19 <95099812+Audi9602@users.noreply.github.com>
2026-04-29 16:51:42 +0530

    Create location.py

---
+++ location.py
@@ -0,0 +1,9 @@
+friend1_location = (19.751480, 75.713890) # Maharashtra
+friend2_location = (27.139759, 80.867531) # Uttar Pradesh
+friend3_location = (22.973423, 78.656891) # Madhya Pradesh
+
+print("Latitude and longitude for Furthest friend's location: ", friend2_location)
+
+all_location = friend1_location + friend2_location + friend3_location
+
+print("All friend's locations are: ", all_location)
